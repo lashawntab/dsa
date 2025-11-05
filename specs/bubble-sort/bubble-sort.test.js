30eefb149e365bb5692ca6716b06a1e9b2be0f82
La Shawnta Beedles <lashawnta@Mac.lan>
2025-11-04 22:06:09 -0600

    bubble sort implementation

--- specs/bubble-sort/bubble-sort.test.js
+++ specs/bubble-sort/bubble-sort.test.js
@@ -11,13 +11,36 @@
 */
 
 function bubbleSort(nums) {
-  // code goes here
+  // if empty return empty
+  // if array is less than 2 elements, return as is
+
+  if (nums.length < 2) {
+    return nums;
+  }
+
+  let swapped = false;
+
+  do {
+    swapped = false;
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] > nums[i + 1]) {
+        let temp = nums[i];
+        nums[i] = nums[i + 1];
+        nums[i + 1] = temp;
+        swapped = true;
+      }
+    }
+  } while (swapped)
+
+  return nums;
+
 }
 
 // unit tests
 // do not modify the below code
-test.skip("bubble sort", function () {
+test("bubble sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   const sortedNums = bubbleSort(nums);
+  console.log(sortedNums)
   expect(sortedNums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 });
